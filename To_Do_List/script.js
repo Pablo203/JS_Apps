16786fb783764aa68c95f7797ed7db9a5b846680
Pablo203 <76450441+Pablo203@users.noreply.github.com>
2021-04-29 13:26:22 +0200

    Minor code change for easier read

--- To_Do_List/script.js
+++ To_Do_List/script.js
@@ -1,13 +1,22 @@
 var x = document.getElementById("topic");
 var y = document.getElementById("content");
+x.value = " ";
+y.value = " ";
 var i = 0;
-function add_item() {
+add_item = () => {
     var topic = x.value;
     var content = y.value;
+    if(x.value == " " || y.value == " "){
+        alert("Please insert valid data");
+    }
+    else{
     document.getElementById("to_do_elements").innerHTML += '<div id=' + i + '><div class = "thing"><div class = "thing-title"><div class = "btn-area"><button class = "delete-btn" onclick="rm_item(' + i + ')"><div class="cross-line-1"></div><div class="cross-line-2"></div></button></div><p>' + topic + '</p></div><div class = "thing-content">' + content + '</div></div></div>';
     i++;
+    x.value = " ";
+    y.value = " ";
+    }
 }
 
-function rm_item(item_id){
+rm_item = (item_id) => {
     document.getElementById(item_id).innerHTML = " ";
 }
